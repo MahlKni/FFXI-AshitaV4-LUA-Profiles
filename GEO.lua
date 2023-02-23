2544fd96914ed8771d137c979025f70c885e5123
MahlKni <adammschaller@gmail.com>
2023-02-23 17:08:43 -0500

    Initial
    Freshish 99 GEO

---
+++ GEO.lua
@@ -0,0 +1,228 @@
+local profile = {};
+local sets = {
+    ['Idle'] = {
+        Main = 'Daybreak',
+        Sub = 'Culminus',
+        Range = { Name = 'Dunna', AugPath='A' },
+        Head = 'Azimuth Hood +2',
+        Neck = 'Sibyl scarf',
+        Ear1 = 'Barkaro. Earring',
+        Ear2 = { Name = 'Azimuth Earring +1', Augment = { [1] = 'Damage taken-4%', [2] = 'Mag. Acc.+12' } },
+        Body = 'Bagua Tunic +1',
+        Hands = 'Geo. Mitaines +2',
+        Ring1 = 'Jhakri Ring',
+        Ring2 = 'Mallquis Ring',
+        Back = { Name = 'Fi Follet Cape +1', AugPath='A' },
+        Waist = 'Isa Belt',
+        Legs = 'Mallquis Trews +2',
+        Feet = 'Geo. Sandals +2',
+    },
+	Nuke = {
+		Neck = 'Mizu. Kubikazari',
+	},
+    Movement = {
+        Feet = 'Geo. Sandals +2',
+    },
+	DT = {
+        Main = 'Malignance Pole',
+		Sub = 'Tokko grip',
+		Neck = 'Loricate torque +1',
+        Ring2 = 'Defending Ring',
+    },
+	TH = { 
+		Ammo = 'Per. Lucky Egg',
+		Waist = 'Chaac Belt',
+	},
+	WS = {
+		Ear1 = { Name = 'Moonshade Earring', Augment = { [1] = 'TP Bonus +250', [2] = '"Mag. Atk. Bns."+4' } },
+		Neck = 'Fotia Gorget',
+		Waist = 'Fotia Belt',
+		Ammo = 'Oshasha\'s Treatise',
+	},
+	WSMulti = {
+		Ear1 = { Name = 'Moonshade Earring', Augment = { [1] = 'TP Bonus +250', [2] = '"Mag. Atk. Bns."+4' } },
+		Neck = 'Fotia Gorget',
+		Waist = 'Fotia Belt',
+		Ammo = 'Oshasha\'s Treatise',
+	},
+	WSMab = {
+		Ear1 = { Name = 'Moonshade Earring', Augment = { [1] = 'TP Bonus +250', [2] = '"Mag. Atk. Bns."+4' } },
+		Neck = 'Fotia Gorget',
+		Waist = 'Fotia Belt',
+		Ammo = 'Oshasha\'s Treatise',
+	},
+	Precast = {
+        Head = 'Nahtirah Hat',
+        Neck = 'Voltsurge Torque',
+        Ear1 = 'Barkaro. Earring',
+		Ear2 = 'Mendi. Earring',
+        Body = { Name = 'Merlinic Jubbah', Augment = { [1] = '"Mag. Atk. Bns."+14', [2] = 'Magic burst dmg.+11%', [3] = 'Mag. Acc.+13' } },
+        Hands = { Name = 'Gende. Gages +1', Augment = { [1] = 'Magic dmg. taken -2%', [2] = 'Phys. dmg. taken -4%', [3] = '"Cure" spellcasting time -4%' } },
+        Ring1 = 'Lebeche ring',
+        Ring2 = 'Mallquis Ring',
+        Back = { Name = 'Fi Follet Cape +1', AugPath='A' },
+        Waist = 'Embla Sash',
+        Legs = 'Doyen pants',
+        Feet = 'Acad. Loafers +1',
+    },
+	Enhancing = {
+        Main = { Name = 'Serenity', Augment = { [1] = '"Cure" spellcasting time -9%', [2] = 'Enha.mag. skill +8', [3] = 'MP+50', [4] = '"Cure" potency +3%' } },
+        Sub = 'Tokko grip',
+		Head = { Name = 'Telchine Cap', Augment = 'Enh. Mag. eff. dur. +9' },
+		Waist = 'Embla Sash', 
+		Body = { Name = 'Chironic Doublet', Augment = { [1] = '"Mag. Atk. Bns."+17', [2] = 'Attack+20', [3] = 'Mag. Acc.+17', [4] = 'Weapon skill damage +1%', [5] = 'Phalanx +5', [6] = 'MND+3', [7] = 'Accuracy+20' } },
+        Legs = { Name = 'Telchine Braconi', Augment = 'Enh. Mag. eff. dur. +8' },
+        Feet = { Name = 'Telchine Pigaches', Augment = 'Enh. Mag. eff. dur. +9' },
+    },
+	Stoneskin = {
+		Neck = 'Nodens Gorget',
+		Waist = 'Siegel Sash',
+	},
+	Cursna = {
+		Feet = 'Gende. Galosh. +1',
+	},
+	Drain = {
+        Main = { Name = 'Rubicundity', Augment = { [1] = 'Dark magic skill +1', [2] = '"Conserve MP"+2', [3] = 'Mag. Acc.+5' } },
+        Sub = 'Culminus',
+        Ring1 = 'Excelsis Ring',
+		Waist = 'Fucho-no-Obi',
+    },
+	Cure = {
+        Main = 'Daybreak',
+        Sub = 'Sors Shield',
+		Head = { Name = 'Vanya Hood', AugPath='C' },
+        Neck = 'Nodens Gorget',
+        Ear1 = 'Static Earring',
+		Ear2 = 'Mendi. Earring',
+        Hands = { Name = 'Telchine Gloves', Augment = { [1] = '"Regen" potency+2', [2] = '"Cure" potency +7%' } },
+		Ring1 = 'Lebeche ring',
+        Ring2 = 'Naji\'s Loop',
+		Body = { Name = 'Chironic Doublet', Augment = { [1] = '"Mag. Atk. Bns."+17', [2] = 'Attack+20', [3] = 'Mag. Acc.+17', [4] = 'Weapon skill damage +1%', [5] = 'Phalanx +5', [6] = 'MND+3', [7] = 'Accuracy+20' } },
+        Feet = { Name = 'Vanya Clogs', AugPath='C' },
+    },
+};
+profile.Sets = sets;
+
+profile.Packer = {
+};
+
+local Settings = {
+    UseTH = false,
+	Burst = true,
+};
+profile.OnLoad = function()
+    gSettings.AllowAddSet = true;
+	
+	(function()
+    AshitaCore:GetChatManager():QueueCommand(1, '/macro book 11');
+    coroutine.sleep(0.1);
+    AshitaCore:GetChatManager():QueueCommand(1, '/macro set 1');
+	coroutine.sleep(0.2);
+	AshitaCore:GetChatManager():QueueCommand(1, '/lockstyleset 17');
+	coroutine.sleep(0.5);
+	AshitaCore:GetChatManager():QueueCommand(1, '/sl blink');
+	end):once(1); --This calls the function after 1 second
+	  
+		
+end
+
+profile.OnUnload = function()
+end
+
+profile.HandleCommand = function(args)
+	if (args[1] == 'TH') then
+        if (Settings.UseTH == true) then
+            Settings.UseTH = false;
+        else
+            Settings.UseTH = true;
+        end
+	elseif (args[1] == 'Burst') then
+		if (Settings.Burst == true) then
+            Settings.Burst = false;
+        else
+            Settings.Burst = true;
+        end
+    end
+end
+
+
+profile.HandleDefault = function()
+    local player = gData.GetPlayer();
+	
+    if (player.Status == 'Engaged') then
+        gFunc.EquipSet(sets.Idle);
+		if (Settings.UseTH == true) then
+            gFunc.EquipSet(sets.TH);
+        end
+		if (Settings.Burst == true) then
+            gFunc.EquipSet(sets.Burst);
+        end
+    elseif (player.Status == 'Resting') then
+        gFunc.EquipSet(sets.Resting);
+    elseif (player.IsMoving == true) then
+		gFunc.EquipSet(sets.Movement);
+    end
+end
+
+profile.HandleAbility = function()
+end
+
+profile.HandleItem = function()
+end
+
+profile.HandlePrecast = function()
+	local spell = gData.GetAction();
+	gFunc.EquipSet(sets.Precast);
+end
+
+profile.HandleMidcast = function()
+	local spell = gData.GetAction();
+	
+	if (spell.Skill == 'Healing Magic') then
+        gFunc.EquipSet(sets.Cure);
+        if string.match(spell.Name, 'Cursna') then
+            gFunc.EquipSet(sets.Cursna);
+        end
+	if (spell.Skill == 'Enhancing Magic') then
+        gFunc.EquipSet(sets.Enhancing);
+        if string.match(spell.Name, 'Phalanx') then
+            gFunc.EquipSet(sets.Phalanx);
+        elseif string.match(spell.Name, 'Stoneskin') then
+            gFunc.EquipSet(sets.Stoneskin);
+        elseif string.contains(spell.Name, 'Regen') then
+            gFunc.EquipSet(sets.Regen);
+        end
+    elseif (spell.Skill == 'Elemental Magic') then
+        gFunc.EquipSet(sets.Nuke);
+		if (Settings.Burst == true) then
+            gFunc.EquipSet(sets.Burst);
+        end
+    elseif (spell.Skill == 'Enfeebling Magic') then
+        gFunc.EquipSet(sets.Enfeebling);
+    elseif (spell.Skill == 'Dark Magic') then
+        gFunc.EquipSet(sets.Enfeebling); -- mostly macc anyways
+        if (string.contains(spell.Name, 'Aspir') or string.contains(spell.Name, 'Drain')) then
+            gFunc.EquipSet(sets.Drain);
+		end
+	end
+	end
+end
+
+profile.HandlePreshot = function()
+end
+
+profile.HandleMidshot = function()
+end
+
+profile.HandleWeaponskill = function()
+	local act = gData.GetAction();
+	if (act.Name == "Black Halo" or act.Name == "Realmrazer") then
+        gFunc.EquipSet(profile.Sets.WSMulti);
+    elseif (act.Name == "Brainshaker" or act.Name == "Skullbreaker" or act.Name == "Rock Crusher" or act.Name == "Vidohunir" or act.Name == "Myrkr" or act.Name == "Shattersoul") then
+        gFunc.EquipSet(profile.Sets.WSMab);
+    else
+        gFunc.EquipSet(profile.Sets.WS);
+    end  
+end
+
+return profile;
